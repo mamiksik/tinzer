157f5dcf4a1490bdb53e7197dc05ca10ba5299b5
renata7zemanova <renata7zemanova@gmail.com>
2016-03-21 16:46:13 +0100

    added game time checker

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "app/api/Engine.h"
 #include "app/control/Control.h"
 #include "app/grid/Position.h"
+#include <chrono>
 
 using namespace std;
 
@@ -14,22 +15,35 @@ int main(int argc, char *argv[])
 {
 	//START OF CONFIG
 	//Control
-	int wheelDiameter = 12;
-	int wheelsDistance = 30;
+	const int wheelDiameter = 12;
+	const int wheelsDistance = 30;
 
 	//Position
-	int width = 7;
-	int height = 7;
+	const int width = 7;
+	const int height = 7;
 
-	int startX = 4;
-	int startY = 4;
-	int startAngle = 0;
+	const int startX = 4;
+	const int startY = 4;
+	const int startAngle = 0;
+	
+	//chrono
+	const int gameTime = 10; // nastavit cas v sekundach 
+	
 	//END OF CONFIG
 
 	Control control = Control(wheelDiameter, wheelsDistance, leftEngine, rightEngine);
 	Position position = Position(width, height, startX, startY, startAngle, control);
 
+	chrono::system_clock::time_point clock_now = chrono::system_clock::now();
+	time_t stopTime = chrono::system_clock::to_time_t(clock_now);
+
+	stopTime = stopTime + gameTime;
+
 	do {
 		//position.moveToPosition(4, 5);
+		chrono::system_clock::time_point clock_now = chrono::system_clock::now();
+		if (chrono::system_clock::to_time_t(clock_now) >= stopTime) {
+			break;
+		}
 	} while (update);
 }
